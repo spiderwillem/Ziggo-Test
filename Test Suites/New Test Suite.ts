c8097c305098d1360ef62e7a8dbb32267e4e0bfc
Ruben <Ruben@DESKTOP-Q8A1BU9.localdomain>
2020-08-01 17:01:28 +0200

    2

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c9388f1c-57bf-4309-ba95-fc74e3accc2f</testSuiteGuid>
+   <testCaseLink>
+      <guid>3c127db0-f984-4da7-871c-9a7dcef22d54</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Open browser and login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
